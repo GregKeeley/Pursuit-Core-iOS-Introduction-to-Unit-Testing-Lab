89ac3a3df816d63798e15af0bd4eebbb97eb2889
Gregory Keeley <gregorykeeley@pursuit.org>
2019-12-02 16:50:27 -0500

    - Initiated lab - Created view controllers for each endpoint - Imported json data

---
+++ IntroToUnitTests/IntroToUnitTests/JokesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JokesViewController.swift
+//  IntroToUnitTests
+//
+//  Created by Gregory Keeley on 12/2/19.
+//  Copyright © 2019 Gregory Keeley. All rights reserved.
+//
+
+import UIKit
+
+class JokesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ IntroToUnitTests/IntroToUnitTestsTests/IntroToUnitTestsTests.swift
@@ -0,0 +1,22 @@
+//
+//  IntroToUnitTestsTests.swift
+//  IntroToUnitTestsTests
+//
+//  Created by Gregory Keeley on 12/2/19.
+//  Copyright © 2019 Gregory Keeley. All rights reserved.
+//
+
+import XCTest
+@testable import IntroToUnitTests
+
+class IntroToUnitTestsTests: XCTestCase {
+
+    override func setUp() {
+
+    }
+
+    override func tearDown() {
+
+    }
+    
+}
